f7e6c5783efde3f3cd6772049b63b1e5104e1fd0
Matthew Pate <mcpate@gmail.com>
2013-07-13 12:57:29 -0700

    Cleans up setup-root.sh for Ubuntu 13.04 try.

--- setup-root.sh
+++ setup-root.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Simple setup.sh for basic Ubuntu instance.  Includes barebones requirements.
+# REQUIREMENTS: Git should first be installed manually so the dotfiles.git repo
+# in this scirpt can be cloned.
+
 
 sudo apt-get update
 
 sudo apt-get -y upgrade
+
+#This includes C compiler among other things
+sudo apt-get install -y build-essential
 
 sudo apt-get install -y vim
 
@@ -11,20 +17,12 @@ sudo apt-get install -y openssh-server
 
 sudo apt-get install -y byobu
 
-#This includes C compiler among other things
-sudo apt-get install -y build-essential
-
 # git pull and install dotfiles
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-  mv dotfiles dotfiles.old
-fi
-if [-d ./.vim/ ]; then
-  mv .vim .vim.old
-fi
+cd Downloads
 git clone https://github.com/mcpate/dotfiles.git
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.vimrc
-ln -sb dotfiles/.vim .
+cd ..
+ln -sb Downloads/dotfiles/.bashrc .
+ln -sb Downloads/dotfiles/.bashrc_custom .
+ln -sb Downloads/dotfiles/.bash_profile .
+ln -sb Downloads/dotfiles/.vimrc .
+ln -sb Downloads/dotfiles/.vim .
