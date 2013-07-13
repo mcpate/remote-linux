4ca24f6e09e8cf4ec8715e34dcc276beac20c918
Matthew Pate <mcpate@gmail.com>
2013-07-12 19:00:36 -0700

    Adds some tools to remote-python.sh.

--- setup-python.sh
+++ setup-python.sh
@@ -4,6 +4,11 @@ install_dir="~/Downloads"
 
 cd install_dir
 
+#important python tools
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y sqlite3 # for the command-line client
+sudo apt-get install -y bzip2 libbz2-dev
+
 #python
 wget http://python.org/ftp/python/3.3.2/Python-3.3.2.tar.bz2
 tar -jxvf Python-3.3.2.tar.bz2
@@ -15,17 +20,17 @@ sudo make install
 
 #python setuptools
 cd install_dir
-sudo apt-get install python-setuptools
+sudo apt-get install -y python-setuptools
 
 #virtualenv
 cd install_dir
 wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.9.1.tar.gz
 tar xvfz virtualenv-1.9.1.tar.gz
-cd virtualenv-1.9.1
+cd virtualenv-1.9.1 
 sudo python3 setup.py install
 
 #pip
-cd ..
+cd install_dir
 wget https://pypi.python.org/packages/source/p/pip/pip-1.3.1.tar.gz
 tar xvfz pip-1.3.1.tar.gz
 cd pip-1.3.1
