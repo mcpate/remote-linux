42da95a70735e3269a0e9bee972a71990749f283
Matt <mcpate@gmail.com>
2013-07-23 22:21:28 -0700

    Update setup-python.sh
    Adds installs.txt usage

--- setup-python.sh
+++ setup-python.sh
@@ -3,6 +3,7 @@
 #            how do I exit if an install fails?
 
 ID=~/Downloads
+LOG=~/installs.txt
 
 cd $ID
 
@@ -11,3 +12,5 @@ sudo apt-get install -y python3.3-dev
 
 #pip
 sudo apt-get install -y python3-pip
+
+echo "python3.3-dev and pip installed" >> $LOG
